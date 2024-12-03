014371b2d66b0769b28901669bd42847441453db
Eugene Rubanov <eugene.rubanov@neo4j.com>
2024-12-03 22:09:40 +0000

    day 3/2

--- src/main/scala/day3/Task3.scala
+++ src/main/scala/day3/Task3.scala
@@ -2,8 +2,10 @@ package day3
 
 import java.io.{CharArrayReader, StringReader}
 import java.util.Scanner
+import scala.collection.mutable.ListBuffer
 import scala.io.Source
 import scala.util.Using
+import scala.util.boundary
 
 @main
 def day3(): Unit =
@@ -11,14 +13,61 @@ def day3(): Unit =
     f.mkString
   }.get
 
+  val nums1 = solveWithRegex(s)
+  println(nums1)
+  println(nums1.sum) // 162813399
+
+  val num2 = solveWithIter(s)
+  println(num2)
+  println(num2.sum) // sample 161 // input 162813399
+  println(solveWithIter(s, true).sum) // sample 48 // 53784220 is too high 
+
+def solveWithRegex(s: String): List[Int] =
   val r = "mul\\((\\d{1,3}),(\\d{1,3})\\)".r
-  var sum = 0
+  val sum = ListBuffer[Int]()
   for (m <- r.findAllIn(s).matchData) {
-    sum += m.group(1).toInt * m.group(2).toInt
+    sum.addOne(m.group(1).toInt * m.group(2).toInt)
   }
-  println(sum)
+  sum.toList
+  
+def solveWithIter(s: String, dos: Boolean = false): List[Int] =
+  val sum2 = ListBuffer[Int]()
+  var i = 0
+  var include = true
+  while (i < s.length) {
+    if (i + 4 < s.length && s.substring(i, i + 4) == "mul(" && include) {
+      var j = i + 4
+      var num1 = 0
+      var num2 = 0
+      boundary:
+        while (j < s.length) {
+          val c = s(j)
+          if (c.isDigit) {
+            num1 = num1 * 10 + c.toString.toInt
+          } else if (c == ',') {
+            num2 = num1
+            num1 = 0
+          } else if (c == ')') {
+            sum2.addOne(num1 * num2)
+            boundary.break()
+          } else {
+            boundary.break()
+          }
+          j += 1
+        }
+      i += 1
+    } else if (i + 4 < s.length && s.substring(i, i + 4) == "do()") {
+      include = true
+      i += 4
+    } else if (i + 7 < s.length && s.substring(i, i + 7) == "don't()" && dos) {
+      include = false
+      i += 7
+    } else {
+      i += 1
+    }
 
-
+  }
+  sum2.toList
 
 
 
